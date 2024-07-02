daa94fdb9967fdb64148943a146d8ab797d41f3e
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-07-02 16:02:16 -0300

    html e css

---
+++ js/scripts2.js
@@ -0,0 +1,16 @@
+// seleção de elementos
+const todoForm = document.querySelector("#add");
+const todoAddTask = document.querySelector("#add-task");
+const todoList = document.querySelector("#todo-list");
+const todoEdit= document.querySelector("#edit");
+const todoEditTask= document.querySelector("#edit-task");
+const cancelEdit = document.querySelector("#edit-cancel");
+
+
+// funções
+
+// eventos
+todoForm.addEventListener ("submit", (e) => {
+    e.preventDefault();
+    console.log("enviou o form");
+})
